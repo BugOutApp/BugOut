867dc8ce61046fee77fa6f7f00cd02cf6197fcd6
Simon Gowing <simongowing1@gmail.com>
2021-05-03 21:44:04 +0200

    refactor: implement react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/login" component={Login}></Route>
+      <Route
+        exact path = "/"
+        compotent={Home}
+      /> 
+        <Route
+        exact path="/login" 
+        component={Login}
+        />
       </BrowserRouter>
     </div>
     
